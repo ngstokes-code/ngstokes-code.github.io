1e26c2611faf8969d7930d67ce05266ce9728757
Nicholas Stokes <ngstokes@buffalo.edu>
2021-02-02 22:25:06 -0500

    Basic homepage created.

---
+++ js/homepage.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+    el: "#background_animation",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xae6fc9
+    // backgroundColor: 0x8d11c2
+  });
